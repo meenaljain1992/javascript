19605261a5b061015fac29d6fc03f009c071f42a
meenal <meenalsaloni82@gmail.com>
2025-03-21 20:34:31 +0530

    new test

---
+++ EX-01-cons-Variables.js
@@ -0,0 +1,13 @@
+const accountID =14467
+let accountemail = "meenal@gmail.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+
+//account = 2 not allowed because const is not changable
+
+console.log(accountID);
+accountemail ="sonal@gmail.com"
+accountpassword = "45676"
+accountcity = "Mumbai"
+console.table([accountID,accountemail,accountpassword,accountcity])
+
