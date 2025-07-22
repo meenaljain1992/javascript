163d01d08948024d47d736383165dc23d3dc7be0
meenal <meenalsaloni82@gmail.com>
2025-07-22 17:35:12 +0530

    datatypes

---
+++ Ex-02-datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat al code as newer version
+//alert(hello) // we are using node js not browser 
+//for documentation using mdm and ECMA script 
+let name ="Meenal"
+let age =32
+let isloggedIn = false
+//number 2 to power 53
+//bigint => for large number
+//string=> single and double both 
+//boolean=>True/false
+//null=> yes is a datatype but standalone value also
+
+//undefined=> value define nhi krte hai
+//let state;
+//let state = null;
+//symbol=> for unique less
+
